821aea8ce303a3cf8fdd9a2c5148b6b1510051e5
Greg Clinker <gregory.clinker@gmail.com>
2025-06-16 16:29:22 +0100

    test commit

--- create-wif.sh
+++ create-wif.sh
@@ -5,34 +5,12 @@ PROJECT_ID="alliance-dev-uk"
 PROJECT_NUMBER="344691125963"
 WORKLOAD_IDENTITY_POOL_ID="projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/github"
 REPO="gregclinker/sixtysix"
-GITHUB_SA="git-hub-actions-sa"
+PROVIDER_NAME="github-cloud-deploy"
 #
 gcloudExec() {
   echo "gcloud $1 --quiet";
   bash -c "gcloud $1 --quiet";
   echo ""
-}
-
-gcloudExec() {
-  echo "gcloud $1 --quiet";
-  echo ""
-  bash -c "gcloud $1 --quiet";
-  echo ""
-}
-
-addRole() {
-  local serviceAccount="$1"
-  local project="$2"
-  shift
-  shift
-  local roles=("$@")
-
-  local roleString=""
-  for role in "${roles[@]}"; do
-    roleString="${roleString} --role=${role}"
-  done
-
-  gcloudExec "projects add-iam-policy-binding $project --member=serviceAccount:$serviceAccount$roleString"
 }
 
 gcloudExec "config set project $PROJECT_ID"
@@ -45,41 +23,22 @@ gcloudExec "config set run/region europe-west2"
 
 #gcloudExec "iam workload-identity-pools providers delete \"${PROVIDER_NAME}\" --workload-identity-pool=github --location=global"
 
-gcloudExec "iam workload-identity-pools providers update-oidc \"github-actions2\" \
+gcloudExec "iam workload-identity-pools providers create-oidc \"${PROVIDER_NAME}\" \
   --project=\"${PROJECT_ID}\" \
   --location=\"global\" \
   --workload-identity-pool=\"github\" \
   --display-name=\"GitHub repo provider\" \
   --attribute-mapping=\"google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository\" \
-  --attribute-condition=\"assertion.repository=='gregclinker/sixtysix'\" \
+  --attribute-condition=\"assertion.repository=='${REPO}'\" \
   --issuer-uri=\"https://token.actions.githubusercontent.com\""
 
-gcloudExec "iam workload-identity-pools providers describe 'github-actions2' \
+gcloudExec "iam workload-identity-pools providers describe '${PROVIDER_NAME}' \
   --project=\"${PROJECT_ID}\" \
   --location=\"global\" \
   --workload-identity-pool=\"github\" \
   --format=\"value(name)\""
 
-gcloudExec "iam service-accounts create \"git-hub-actions-sa\" --project \"${PROJECT_ID}\""
-addRole "git-hub-actions-sa@${PROJECT_ID}.iam.gserviceaccount.com" "$PROJECT_ID" "roles/run.admin" "roles/artifactregistry.admin" "roles/iam.serviceAccountUser"
-
-# ${REPO} is the full repo name including the parent GitHub organization,
-# such as "my-org/my-repo".
-#
-# ${WORKLOAD_IDENTITY_POOL_ID} is the full pool id, such as
-# "projects/123456789/locations/global/workloadIdentityPools/github".
-gcloudExec "iam service-accounts add-iam-policy-binding \"${GITHUB_SA}@${PROJECT_ID}.iam.gserviceaccount.com\" \
+gcloudExec "iam service-accounts add-iam-policy-binding \"${PROJECT_NUMBER}-compute@developer.gserviceaccount.com\" \
   --project=\"${PROJECT_ID}\" \
   --role=\"roles/iam.workloadIdentityUser\" \
-  --member=\"principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}\""
-
-gcloudExec "run services add-iam-policy-binding \"test-1\" \
-  --project=\"${PROJECT_ID}\" \
-  --role=\"roles/run.admin\" \
-  --member=\"principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}\""
-
-gcloudExec "artifacts repositories add-iam-policy-binding \"cloud-run-source-deploy\" \
-  --location=\"europe-west2\" \
-  --project=\"${PROJECT_ID}\" \
-  --role=\"roles/artifactregistry.admin\" \
   --member=\"principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}\""
